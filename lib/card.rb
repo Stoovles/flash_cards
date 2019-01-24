c457c10c6a135f8ee55fc75141a206b9b029bf0a
Ethan Grab <ethangrab@gmail.com>
2019-01-23 21:56:09 -0700

    Turn Class with initializers

--- lib/card.rb
+++ lib/card.rb
@@ -1,23 +1,34 @@
 class Card
   attr_accessor :question, :answer, :category
+
   def initialize(question, answer, category)
     @question = question
     @answer = answer
     @category = category
   end
+end
 
-  def ask_question
-    puts question
+class Turn
+  attr_accessor :guess, :card_name
+
+  def initialize(guess, card_name)
+    @guess = guess
+    @card_name = card_name
   end
 
-  def take_user_answer_and_check
-    user_answer = gets.chomp
-    if user_answer == answer
-      "That's correct!"
-      num_correct =+ 1
-    else
-      "That's incorrect."
-      num_incorrect =+ 1
-    end
-  end
+  # def ask_question
+  #   puts question
+  # end
+  #
+  # def take_user_answer_and_check
+  #   user_answer = gets.chomp
+  #   if user_answer == answer
+  #     "That's correct!"
+  #     num_correct =+ 1
+  #   else
+  #     "That's incorrect."
+  #     num_incorrect =+ 1
+  #   end
+  # end
+
 end
