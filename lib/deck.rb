5b7fc88ac0b4cb466da702eb3e1de55a726887da
Ethan Grab <ethangrab@gmail.com>
2019-01-24 13:13:44 -0700

    deck class fixed cards_in_category

--- lib/deck.rb
+++ lib/deck.rb
@@ -1,15 +1,23 @@
 class Deck
-  attr_accessor :cards
+  attr_accessor :cards, :category_array
 
   def initialize(cards)
     @cards = cards
+    @category_array = []
   end
 
   def card_count
     return cards.count
   end
 
-  def cards_in_category(category)
-    cards.find_all {|card_category| card_category.include?(category)}
+  def cards_in_category(check_category)
+    cards.each do |card|
+      require 'pry';binding.pry
+      if card.category == check_category
+        @category_array << card
+      end
+    end
   end
+
+
 end
