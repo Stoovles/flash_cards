62327219173a5a9be4b1537429ac189c7e244016
Ethan Grab <ethangrab@gmail.com>
2019-01-25 09:56:14 -0700

    Round class working through

---
+++ lib/round.rb
@@ -0,0 +1,13 @@
+class Round
+  attr_accessor :deck, :turns, :current_card
+
+  def initialize(deck)
+    @deck = deck
+    @turns = []
+    @current_card = current_card
+  end
+
+  def take_turn
+
+
+end
